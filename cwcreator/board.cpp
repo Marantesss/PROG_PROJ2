9d55cb6267f36c7654763283b2b5886a79355340
Tiago Silva <tiago.silva.99@hotmail.com>
2018-04-30 19:56:28 +0100

    board iniciada

--- cwcreator/board.cpp
+++ cwcreator/board.cpp
@@ -1,22 +1,41 @@
 #include "board.h"
+#include <vector>
+#include <iostream>
 
-Board::Board(int &columns, int &lines)
+Board::Board(int columns, int lines)
 {
-	this->columns = columns;
-	this->lines = lines;
-}
+	numColumns = columns;
+	numLines = lines;
+	
+	board.resize(numLines);
+	for (int i = 0; i < numLines; i++)
+		board[i].resize(numColumns);
+
+
+	for (int i = 0; i < numLines; i++)
+		for (int j = 0; j < numColumns; j++)
+			board.at(i).at(j) = '.';
+
+
+}	
 
 int Board::getColumns() const
 {
-	return columns;
+	return numColumns;
 }
 
 int Board::getLines() const
 {
-	return lines;
+	return numLines;
 }
 
 void Board::showBoard()
 {
+	for (int i = 0; i < numLines; i++)
+	{
+		for (int j = 0; j < numColumns; j++)
+			cout << board.at(i).at(j) << " ";
+		cout << endl;
+	}
 
 }
--- cwcreator/board.h
+++ cwcreator/board.h
@@ -1,18 +1,22 @@
 #ifndef board_h
 #define board_h
 #include <string>
+#include <vector>
+
 
 using namespace std;
 
 class Board {
 public:
-	Board(int &columns,int &lines); // Atualiza o numero de colunas de linhas
+	Board(int columns,int lines); // Atualiza o numero de colunas de linhas
 	int getColumns() const; // Devolve o numero de colunas
 	int getLines() const; // Devolve o numero de linhas
 	void showBoard(); // Da print ao tabuleiro
 private:
-	int columns; // Numero de colunas
-	int lines; // Numero de linhas
+	int numColumns; // Numero de colunas
+	int numLines; // Numero de linhas
+	vector<vector<char>> board;
+
 };
 
 #endif
